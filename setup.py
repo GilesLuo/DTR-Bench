5c2af3521ac7526e8e574b4ec8025064c2cc6197
Zhiyao Luo <luozhiyao933@126.com>
2024-03-19 12:11:00 +0000

    0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='DTRBench',
-    version='0.0.1',
+    version='0.0.2',
     author='Zhiyao Luo, Mingcheng Zhu',
     author_email='zhiyao.luo@eng.ox.ac.uk',
     description='DTR-Bench: An in silico Environment and Benchmark Platform for Reinforcement Learning Based '
@@ -10,6 +10,6 @@ setup(
     packages=find_packages(),
     install_requires=[
         "DTRGym",
-        "optuna=3.2.0",
+        "optuna==3.2.0",
     ],
 )
